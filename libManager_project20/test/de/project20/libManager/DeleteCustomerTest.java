d286e49fdc2a4b5b55af5f0e631c4572964417c9
KenoScheffold <kenoscheffold@gmail.com>
2024-05-14 16:03:36 +0200

    Add files via upload

---
+++ libManager_project20/test/de/project20/libManager/DeleteCustomerTest.java
@@ -0,0 +1,14 @@
+package de.project20.libManager;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class DeleteCustomerTest {
+
+	@Test
+	void testDeleteCustomer() {
+		
+	}
+
+}

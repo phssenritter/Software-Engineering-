7eb6045d7542dae2fe0bc70b9ffecf3ef5c91299
X5Rocky1 <2091rocky@gmail.com>
2024-05-14 16:32:34 +0200

    testRemoveBook added (noch nicht fertig)

--- libManager_project20/test/de/project20/libManager/BookTest.java
+++ libManager_project20/test/de/project20/libManager/BookTest.java
@@ -9,10 +9,5 @@ public class BookTest {
     public void testRemoveBook(){
         books.remove("2947501718342");
         assertNull(books.getBook("2947501718342"));
-
-
-
-        //books.add(new Book("2947501718342", "How to get rich", authors, 1985, "Berlin", "Klett Verlag", 1));
-
     }
 }

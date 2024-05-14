177071751d6adb232c6a638160909c7e8609a6cc
KenoScheffold <kenoscheffold@gmail.com>
2024-05-14 17:50:24 +0200

    Add files via upload

---
+++ libManager_project20/src/de/project20/libManager/BookCopy.java
@@ -0,0 +1,59 @@
+package de.project20.libManager;
+
+import java.util.Date;
+
+public class BookCopy {
+	private long id;
+	private Book book;
+	private String shelfLocation;
+	
+	public BookCopy(final long id, final Book book, final String shelfLocation) {
+		if(id <= 0 || book == null || shelfLocation == null) {
+			throw new IllegalArgumentException("parameters must not be null and id must not be negative or zero");
+		}
+		this.id = id;
+		this.book = book;
+		this.shelfLocation = shelfLocation;
+	}
+	
+	private Date addedToLibrary() {
+		//TODO;
+		return null;
+	}
+	
+	private boolean lent() {
+		//TODO;
+		return true;
+	}
+	
+	private Date lentDate() {
+		//TODO;
+		return null;
+	}
+
+	public final long getId() {
+		return id;
+	}
+
+	public final void setId(long id) {
+		this.id = id;
+	}
+
+	public final Book getBook() {
+		return book;
+	}
+
+	public final void setBook(Book book) {
+		this.book = book;
+	}
+
+	public final String getShelfLocation() {
+		return shelfLocation;
+	}
+
+	public final void setShelfLocation(String shelfLocation) {
+		this.shelfLocation = shelfLocation;
+	}
+	
+	
+}
---
+++ libManager_project20/test/de/project20/libManager/DeleteBookTest.java
@@ -0,0 +1,20 @@
+package de.project20.libManager;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.ByteArrayInputStream;
+
+import org.junit.Before;
+import org.junit.jupiter.api.Test;
+
+class DeleteBookTest {
+	
+	/*
+	 * Testing the usual case where a customer with the ID is present. 
+	 */
+	@Test
+	void testDeleteBook() {
+
+	}
+
+}
